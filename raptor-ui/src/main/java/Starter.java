02161c40521585056c7118e448b0711324867ad8
AnantPawar <anantcp2010@gmail.com>
2015-10-30 00:39:47 +0530

    moved routes to seperate class

--- raptor-ui/src/main/java/Starter.java
+++ raptor-ui/src/main/java/Starter.java
@@ -1,6 +1,7 @@
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
+import setup.RouteConfig;
 
 /**
  * Created by Anant on 22-10-2015.
@@ -17,70 +18,10 @@ public class Starter {
     {
         Vertx vertx = Vertx.vertx();
         Router router = Router.router(vertx);
+        RouteConfig routeConfig = new RouteConfig(router);
 
+        routeConfig.initRoutes();
         HttpServer httpServer = vertx.createHttpServer();
-
-        router.route("/").handler(routingContext -> {
-            routingContext
-                    .response()
-                    .sendFile("./web/home.html");
-        });
-
-        // route for css files
-        router.route("/lib/bootstrap/css/:stylesheet").handler(routingContext -> {
-            routingContext
-                    .response()
-                    .sendFile("web/lib/bootstrap/css/" + routingContext
-                            .request()
-                            .params()
-                            .get("stylesheet"));
-        });
-
-        router.route("/css/:stylesheet").handler(routingContext -> {
-            routingContext
-                    .response()
-                    .sendFile("web/css/" + routingContext
-                            .request()
-                            .params()
-                            .get("stylesheet"));
-        });
-
-        // route for js files
-        router.route("/lib/bootstrap/js/:javascript").handler(routingContext -> {
-            routingContext
-                    .response()
-                    .sendFile("web/lib/bootstrap/js/" + routingContext
-                            .request()
-                            .params()
-                            .get("javascript"));
-        });
-
-        router.route("/lib/jquery/:javascript").handler(routingContext -> {
-            routingContext
-                    .response()
-                    .sendFile("web/lib/jquery/" + routingContext
-                            .request()
-                            .params()
-                            .get("javascript"));
-        });
-
-        router.route("/lib/html5shiv/:javascript").handler(routingContext -> {
-            routingContext
-                    .response()
-                    .sendFile("web/lib/html5shiv/" + routingContext
-                            .request()
-                            .params()
-                            .get("javascript"));
-        });
-
-        router.route("/lib/respond/:javascript").handler(routingContext -> {
-            routingContext
-                    .response()
-                    .sendFile("web/lib/respond/" + routingContext
-                            .request()
-                            .params()
-                            .get("javascript"));
-        });
 
         httpServer
                 .requestHandler(router::accept)
---
+++ raptor-ui/src/main/java/setup/RouteConfig.java
@@ -0,0 +1,81 @@
+package setup;
+
+import io.vertx.ext.web.Router;
+
+/**
+ * Created by Anant on 30-10-2015.
+ */
+public class RouteConfig {
+    private Router router;
+
+    public RouteConfig(Router router)
+    {
+        this.router = router;
+    }
+
+    public void initRoutes()
+    {
+        router.route("/").handler(routingContext -> {
+            routingContext
+                    .response()
+                    .sendFile("./web/home.html");
+        });
+
+        // route for css files
+        router.route("/lib/bootstrap/css/:stylesheet").handler(routingContext -> {
+            routingContext
+                    .response()
+                    .sendFile("web/lib/bootstrap/css/" + routingContext
+                            .request()
+                            .params()
+                            .get("stylesheet"));
+        });
+
+        router.route("/css/:stylesheet").handler(routingContext -> {
+            routingContext
+                    .response()
+                    .sendFile("web/css/" + routingContext
+                            .request()
+                            .params()
+                            .get("stylesheet"));
+        });
+
+        // route for js files
+        router.route("/lib/bootstrap/js/:javascript").handler(routingContext -> {
+            routingContext
+                    .response()
+                    .sendFile("web/lib/bootstrap/js/" + routingContext
+                            .request()
+                            .params()
+                            .get("javascript"));
+        });
+
+        router.route("/lib/jquery/:javascript").handler(routingContext -> {
+            routingContext
+                    .response()
+                    .sendFile("web/lib/jquery/" + routingContext
+                            .request()
+                            .params()
+                            .get("javascript"));
+        });
+
+        router.route("/lib/html5shiv/:javascript").handler(routingContext -> {
+            routingContext
+                    .response()
+                    .sendFile("web/lib/html5shiv/" + routingContext
+                            .request()
+                            .params()
+                            .get("javascript"));
+        });
+
+        router.route("/lib/respond/:javascript").handler(routingContext -> {
+            routingContext
+                    .response()
+                    .sendFile("web/lib/respond/" + routingContext
+                            .request()
+                            .params()
+                            .get("javascript"));
+        });
+
+    }
+}
